2995162804d3f4a6a6960b80d6404452c4728604
kazimovzaman2 <kazimovzaman2@gmail.com>
2022-12-27 12:11:45 +0400

    Create New app named api

---
+++ backend/cfehome/api/views.py
@@ -0,0 +1,21 @@
+import json
+from django.http import JsonResponse
+
+def api_home(request, *args, **kwargs):
+    print(request.GET) # url query params
+    print(request.POST)
+
+    body = request.body # byte string of JSON data
+    data = {}
+
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dict
+    except:
+        pass
+
+    print(data)
+    data['params'] = dict(request.GET)
+    data['headers'] = dict(request.headers)
+    data['content_type'] = request.content_type
+    print(data)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,15 +6,15 @@ import requests
 
 # endpoint = "https://httpbin.org/status/200/"
 # endpoint = "https://httpbin.org/anything"
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, json={"query": "Hello World"})
-print(get_response.text)
-print(get_response.status_code)
-
-
+get_response = requests.get(endpoint, params={"product_id": 123})
+# print(get_response.text)
+# print(get_response.status_code)
 
 
-# print(get_response.json())
+
+
+print(get_response.json())
 # print(get_response.status_code)
 
