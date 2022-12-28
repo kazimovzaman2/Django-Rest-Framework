ad4f996bcfe9b91dfc381756ca78af7489b752b2
kazimovzaman2 <kazimovzaman2@gmail.com>
2022-12-28 19:01:07 +0400

    Add delete and update to api

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -23,11 +23,37 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title 
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        super().perform_destroy(instance)
+
+product_destroy_view = ProductDestroyAPIView.as_view()
 
 
 # class ProductListAPIView(generics.ListAPIView):
