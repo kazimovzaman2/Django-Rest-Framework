ab269e65fad7ed885bdc0e954b1b07d85a00a384
kazimovzaman2 <kazimovzaman2@gmail.com>
2022-12-28 12:55:52 +0400

    Edit some files

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,6 +1,4 @@
-import json
 from django.forms.models import model_to_dict
-# from django.http import JsonResponse, HttpResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,17 +6,15 @@ from rest_framework.response import Response
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(['GET'])
+
+@api_view(['POST'])
 def api_home(request, *args, **kwargs):
     """
     DRF API View
     """
-    instance = Product.objects.all().order_by("?").first()
-    data = {}
-    if instance:
-        # data = model_to_dict(instance, fields=['id', 'title', 'price', 'sale_price'])
-        data = ProductSerializer(instance).data
-    return Response(data)
-    #     data = dict(data)
-    #     json_data_str = json.dumps(data)
-    # return HttpResponse(json_data_str, headers={"content-type": "application/json"})
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid(raise_exception=True):
+        # instance = serializer.save()
+        print(serializer.data)
+        return Response(serializer.data)
+    return Response({"invalid": "not good data"}, status=400)
--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -6,7 +6,17 @@ class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = Product
-        fields = ['title', 'content', 'price', 'sale_price', 'my_discount']
+        fields = [
+                    'title', 
+                    'content', 
+                    'price', 
+                    'sale_price', 
+                    'my_discount'
+                    ]
 
     def get_my_discount(self, obj):
+        if not hasattr(obj, 'id'):
+            return None
+        if not isinstance(obj, Product):
+            return None
         return obj.get_discount()
