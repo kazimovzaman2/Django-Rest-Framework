28469e5678a57ae663ec0ab301ddaed547d7ca7e
kazimovzaman2 <kazimovzaman2@gmail.com>
2022-12-28 18:38:51 +0400

    Addmin not_found.py file. Function based api view

--- backend/cfehome/products/urls.py
+++ backend/cfehome/products/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from . import views
 
+# /api/products/
 urlpatterns = [
     path("", views.product_list_create_view),
+    path('<int:pk>/update/', views.product_detail_view),
+    path('<int:pk>/delete/', views.product_detail_view),
     path('<int:pk>/', views.product_detail_view)
 ]
