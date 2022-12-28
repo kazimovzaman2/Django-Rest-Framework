8b3f346c1c48fabe3c9c0e3679da41f8ca157ca5
kazimovzaman2 <kazimovzaman2@gmail.com>
2022-12-28 20:38:19 +0400

    Mixin views

--- backend/cfehome/products/views.py
+++ backend/cfehome/products/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -6,6 +6,9 @@ from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
+
+
+"""       Generic view      """
 
 # List and Create View page
 class ProductListCreateAPIView(generics.ListCreateAPIView):
@@ -73,11 +76,64 @@ product_destroy_view = ProductDestroyAPIView.as_view()
 
 
 
+"""       Mixin View       """
 
 
 
 
-# Function based view
+class ProductMixinView(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    mixins.RetrieveModelMixin,
+    generics.GenericAPIView
+    ):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+    
+    def get(self, request, *args, **kwargs):    # HTTP -> GET
+        pk = kwargs.get("pk")
+        if pk is not None:
+            return self.retrieve(request, *args, **kwargs)
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):    # HTTP -> POST
+        return self.create(request, *args, **kwargs)
+
+    def perform_create(self, serializer):
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = "this is a single view doing cool stuff"
+        serializer.save(content=content)
+
+product_mixin_view = ProductMixinView.as_view()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""      Function based view       """
+
 @api_view(['GET', 'POST'])
 def product_alt_view(request, pk=None, *args, **kwargs):
     method = request.method
