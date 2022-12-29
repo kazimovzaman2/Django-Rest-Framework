a142d604a322960bfc2d890b7579ba075cd357cb
kazimovzaman2 <kazimovzaman2@gmail.com>
2022-12-29 09:28:06 +0400

    Createed authentication.py file

--- py_client/create.py
+++ py_client/create.py
@@ -1,5 +1,7 @@
 import requests
 
+
+headers = {'Authorization': 'Bearer d212e28cb3835f557b4fe0e2e09ee151b205321a'}
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
@@ -7,5 +9,5 @@ data = {
     "price": 32.99
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
