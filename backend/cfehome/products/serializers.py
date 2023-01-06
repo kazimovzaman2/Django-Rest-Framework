020d599746b91fb4d39c8aac3368461ad2238e93
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-01-06 19:56:43 +0400

    Validation

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -2,6 +2,7 @@ from rest_framework import serializers
 from rest_framework.reverse import reverse
 
 from .models import Product
+from . import validators
 
 class ProductSerializer(serializers.ModelSerializer):
     my_discount = serializers.SerializerMethodField(read_only=True)
@@ -10,20 +11,33 @@ class ProductSerializer(serializers.ModelSerializer):
             view_name="product-detail",
             lookup_field = 'pk'
     )
+    title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
+    # name = serializers.CharField(source='title', read_only=True)
+    # email = serializers.CharField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
                     'url',
                     'edit_url',
                     'pk',
-                    'title', 
+                    'title',
+                    'name',
                     'content', 
                     'price', 
                     'sale_price', 
                     'my_discount'
                     ]
+    
+    # Validate Data
+    # def validate_title(self, value):
+    #     request = self.context.get('request')
+    #     user = request.user
+    #     qs = Product.objects.filter(user=user, title__iexact=value)
+    #     if qs.exists():
+    #         raise serializers.ValidationError(f"{value} is already a product name")
+    #     return value
 
-
+    # Email Field
     # def create(self, validated_data):
     #     # email = validated_data.pop('email')
     #     obj = super().create(validated_data)
