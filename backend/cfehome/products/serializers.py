e9e1e0fb8d5cc619d7622f0c9c64ddf7cc0942a7
kazimovzaman2 <kazimovzaman2@gmail.com>
2023-01-08 08:16:41 +0400

    Foreign key

--- backend/cfehome/products/serializers.py
+++ backend/cfehome/products/serializers.py
@@ -1,10 +1,27 @@
 from rest_framework import serializers
 from rest_framework.reverse import reverse
 
+from api.serializers import UserPublicSerializer
 from .models import Product
 from . import validators
 
+
+
+
+class ProductInlineSerializer(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(
+            view_name="product-detail",
+            lookup_field = 'pk',
+            read_only = True,
+    )
+    title = serializers.CharField(read_only=True)
+
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    owner = UserPublicSerializer(source='user', read_only=True)
+    related_products = ProductInlineSerializer(source='user.product_set.all', read_only=True, many=True)
+    my_user_data = serializers.SerializerMethodField(read_only=True)
     my_discount = serializers.SerializerMethodField(read_only=True)
     edit_url = serializers.SerializerMethodField(read_only=True)
     url = serializers.HyperlinkedIdentityField(
@@ -13,11 +30,12 @@ class ProductSerializer(serializers.ModelSerializer):
     )
     title = serializers.CharField(validators=[validators.validate_title_no_hello, validators.unique_product_title])
     # name = serializers.CharField(source='title', read_only=True)
-    # email = serializers.CharField(source='user.email', read_only=True)
+    # email = serializers.EmailField(source='user.email', read_only=True)
     class Meta:
         model = Product
         fields = [
-                    # 'user',
+                    'owner',
+                    # 'email',
                     'url',
                     'edit_url',
                     'pk',
@@ -26,9 +44,16 @@ class ProductSerializer(serializers.ModelSerializer):
                     'content', 
                     'price', 
                     'sale_price', 
-                    'my_discount'
+                    'my_discount',
+                    'my_user_data',
+                    'related_products',
                     ]
     
+    def get_my_user_data(self, obj):
+        return {
+            "username": obj.user.username
+        }
+
     # Validate Data
     # def validate_title(self, value):
     #     request = self.context.get('request')
